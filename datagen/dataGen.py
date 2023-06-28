dc086fdab92d10dbcfe925b8c6dde3e47bf823d0
zouxfky <zouxf@uniplore.io>
2023-06-28 15:07:45 +0800

    111232

---
+++ datagen/dataGen.py
@@ -0,0 +1,302 @@
+import csv
+import uuid
+import random
+import numpy as np
+import util.dataDictionary as dataDictionary
+
+COMMENT_HEADER = ['comment_id', 'text', 'create_time']
+CONCEPT_HEADER = ['concept_id', 'concept_name', 'context']
+COURSE_HEADER = ['course_id', 'course_name', 'prerequisites', 'about', 'resource']
+OTHER_HEADER = ['other_id', 'type', 'content']
+OTHER_TYPE = ['zhihu', 'baike', 'wiki']
+PAPER_HEADER = ['paper_id', 'title', 'abstract', 'doi', 'lang', 'num_citation', 'pages', 'score', 'sourcetype', 'urls', 'year']
+PROBLEM_HEADER = ['problem_id', 'content', 'option', 'answer', 'score', 'type', 'typetext', 'location', 'context_id', 'language']
+REPLY_HEADER = ['reply_id', 'text', 'create_time']
+TEACHER_HEADER = ['teacher_id', 'name', 'about', 'job_title', 'org_name']
+USER_HEADER = ['user_id', 'name', 'gender', 'school', 'year_of_birth']
+VIDEO_HEADER = ['video_id', 'name', 'start', 'end', 'text']
+GRAPH_HEADER = ['start_id', 'end_id']
+USER_COURSE_HEADER = ['user_id', 'course']
+USER_PROBLEM_HEADER = ['user_id', 'problem']
+USER_VIDEO_HEADER = ['user_id', 'seq']
+
+
+def commentGen(dir, row_data, scale):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        total = 0
+        writer = csv.writer(a)
+        writer.writerow(COMMENT_HEADER)
+        while total <= scale:
+            writer.writerow([uuid.uuid1(), dataDictionary.getRandomData(row_data)[0], dataDictionary.getRandomTime()])
+            total = total + 1
+
+
+def conceptGen(dir, row_data, scale):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        total = 0
+        writer = csv.writer(a)
+        writer.writerow(CONCEPT_HEADER)
+        while total <= scale:
+            data = dataDictionary.getRandomData(row_data)
+            writer.writerow([uuid.uuid1(), data[0], data[2]])
+            total = total + 1
+
+
+def courseGen(dir, row_data, scale):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        total = 0
+        writer = csv.writer(a)
+        writer.writerow(COURSE_HEADER)
+        while total <= scale:
+            data = dataDictionary.getRandomData(row_data)
+            writer.writerow([uuid.uuid1(), data[1], data[2], data[3], data[4]])
+            total = total + 1
+
+
+def problemGen(dir, row_data, scale):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        total = 0
+        writer = csv.writer(a)
+        writer.writerow(PROBLEM_HEADER)
+        while total <= scale:
+            data = dataDictionary.getRandomData(row_data)
+            data[0] = uuid.uuid1()
+            writer.writerow(data)
+            total = total + 1
+
+
+def replyGen(dir, row_data, scale):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        total = 0
+        writer = csv.writer(a)
+        writer.writerow(REPLY_HEADER)
+        while total <= scale:
+            writer.writerow([uuid.uuid1(), dataDictionary.getRandomData(row_data)[0], dataDictionary.getRandomTime()])
+            total = total + 1
+
+
+def teacherGen(dir, row_data, scale):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        total = 0
+        writer = csv.writer(a)
+        writer.writerow(TEACHER_HEADER)
+        while total <= scale:
+            data = dataDictionary.getRandomData(row_data)
+            writer.writerow([uuid.uuid1(), dataDictionary.getRandomName(), data[3], data[4], data[5]])
+            total = total + 1
+
+
+def userGen(dir, row_data, scale):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        total = 0
+        writer = csv.writer(a)
+        writer.writerow(USER_HEADER)
+        while total <= scale:
+            school = dataDictionary.getRandomData(row_data)
+            writer.writerow([uuid.uuid1(), dataDictionary.getRandomName(), random.choice([0.0, 1.0]), school[1],
+                             random.randint(1960, 2010)])
+            total = total + 1
+
+
+def videoGen(dir, row_data, scale):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        total = 0
+        writer = csv.writer(a)
+        writer.writerow(VIDEO_HEADER)
+        while total <= scale:
+            data = dataDictionary.getRandomData(row_data)
+            writer.writerow([uuid.uuid1(), data[1], data[2], data[3], data[4]])
+            total = total + 1
+
+
+def courseTeacherGen(dir, course, teacher):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        writer = csv.writer(a)
+        writer.writerow(GRAPH_HEADER)
+        for row in course.iterrows():
+            data = dataDictionary.getRandomData(teacher)
+            writer.writerow([row[1]["course_id"], data[0]])
+
+
+def courseFiledGen(dir, course, field):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        writer = csv.writer(a)
+        writer.writerow(GRAPH_HEADER)
+        for row in course.iterrows():
+            count = random.randint(1, 3)
+            data = []
+            if count == 1:
+                writer.writerow([row[1]["course_id"], dataDictionary.getRandomData(field)[1]])
+            else:
+                for i in range(count):
+                    data.append(dataDictionary.getRandomData(field)[1])
+                writer.writerow([row[1]["course_id"], data])
+
+
+def courseCommentGen(dir, course, comment):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        writer = csv.writer(a)
+        writer.writerow(GRAPH_HEADER)
+        for row in comment.iterrows():
+            data = dataDictionary.getRandomData(course)
+            writer.writerow([data[0], row[1]["comment_id"]])
+
+
+def userCourseGen(dir, user, course):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        writer = csv.writer(a)
+        writer.writerow(USER_COURSE_HEADER)
+        for row in user.iterrows():
+            count = random.randint(1, 8)
+            data = []
+            if count == 1:
+                time = dataDictionary.getRandomTime()
+                writer.writerow([row[1]["user_id"], {"course_id": dataDictionary.getRandomData(course)[0],
+                                                     "enroll_time": str(time)}])
+            else:
+                for i in range(count):
+                    time = dataDictionary.getRandomTime()
+                    data.append({"course_id": dataDictionary.getRandomData(course)[0],
+                                 "enroll_time": str(time)})
+                writer.writerow([row[1]["user_id"], data])
+
+
+def userProblemGen(dir, user, problem):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        writer = csv.writer(a)
+        writer.writerow(USER_PROBLEM_HEADER)
+        for row in user.iterrows():
+            count = random.randint(0, 60)
+            problem_list = []
+            if count == 1:
+                data = dataDictionary.getRandomData(problem)
+                writer.writerow([row[1]["user_id"], {"problem_id": data[0],
+                                                     "is_correct": random.randint(0, 1),
+                                                     "attempts": random.randint(1, 50),
+                                                     "score": random.randint(0, 100),
+                                                     "submit_time": str(dataDictionary.getRandomTime())}])
+            elif count > 1:
+                for i in range(count):
+                    data = dataDictionary.getRandomData(problem)
+                    problem_list.append({"problem_id": data[0],
+                                         "is_correct": random.randint(0, 1),
+                                         "attempts": random.randint(1, 50),
+                                         "score": random.randint(0, 100),
+                                         "submit_time": str(dataDictionary.getRandomTime())})
+                writer.writerow([row[1]["user_id"], problem_list])
+
+
+def userVideoGen(dir, user, video):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        writer = csv.writer(a)
+        writer.writerow(USER_VIDEO_HEADER)
+        for row in user.iterrows():
+            video_count = random.randint(0, 7)
+            segment_count = random.randint(1, 5)
+            segment_list = []
+            video_list = []
+            if video_count == 0:
+                continue
+            for x in range(video_count):
+                data = dataDictionary.getRandomData(video)
+                start_time = dataDictionary.getRandomTime()
+                time = start_time
+                if segment_count == 1:
+                    start_point = round(random.uniform(0, 2000), 1)
+                    end_point = round(start_point + random.uniform(0, 200))
+                    segment_list = {"start_point": start_point,
+                                    "end_point": end_point,
+                                    "speed": random.choice([0.5, 1.0, 1.5, 2.0]),
+                                    "local_start_time": time.timestamp()}
+                else:
+                    for y in range(segment_count):
+                        start_point = round(random.uniform(0, 2000), 1)
+                        end_point = round(start_point + random.uniform(0, 200))
+                        if y != 0:
+                            time = dataDictionary.getNextTime(time)
+                        segment_list.append({"start_point": start_point,
+                                             "end_point": end_point,
+                                             "speed": random.choice([0.5, 1.0, 1.5, 2.0]),
+                                             "local_start_time": time.timestamp()})
+                if video_count == 1:
+                    video_list = {"video_id": data[0], "seq": segment_list}
+                else:
+                    video_list.append({"video_id": data[0],
+                                       "seq": segment_list})
+            writer.writerow([row[1]["user_id"], video_list])
+
+
+def userCommentGen(dir, user, comment):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        writer = csv.writer(a)
+        writer.writerow(GRAPH_HEADER)
+        for row in comment.iterrows():
+            data = dataDictionary.getRandomData(user)
+            writer.writerow([data[0], row[1]["comment_id"]])
+
+
+def userReplyGen(dir, user, reply):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        writer = csv.writer(a)
+        writer.writerow(GRAPH_HEADER)
+        for row in reply.iterrows():
+            data = dataDictionary.getRandomData(user)
+            writer.writerow([data[0], row[1]["reply_id"]])
+
+
+def commentReplyGen(dir, comment, reply):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        writer = csv.writer(a)
+        writer.writerow(GRAPH_HEADER)
+        for row in reply.iterrows():
+            data = dataDictionary.getRandomData(comment)
+            writer.writerow([data[0], row[1]["reply_id"]])
+
+
+def conceptCommentGen(dir, concept, comment):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        writer = csv.writer(a)
+        writer.writerow(GRAPH_HEADER)
+        for row in comment.iterrows():
+            data = dataDictionary.getRandomData(concept)
+            writer.writerow([data[0], row[1]["comment_id"]])
+
+
+def conceptCourseGen(dir, concept, course):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        writer = csv.writer(a)
+        writer.writerow(GRAPH_HEADER)
+        for row in course.iterrows():
+            for i in range(random.randint(1, 8)):
+                data = dataDictionary.getRandomData(concept)
+                writer.writerow([data[0], row[1]["course_id"]])
+
+
+def conceptProblemGen(dir, concept, problem):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        writer = csv.writer(a)
+        writer.writerow(GRAPH_HEADER)
+        for row in concept.iterrows():
+            for i in range(random.randint(0, 8)):
+                data = dataDictionary.getRandomData(problem)
+                writer.writerow([row[1]["concept_id"], data[0]])
+
+
+def conceptVideoGen(dir, concept, video):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        writer = csv.writer(a)
+        writer.writerow(GRAPH_HEADER)
+        for row in concept.iterrows():
+            for i in range(random.randint(1, 3)):
+                data = dataDictionary.getRandomData(video)
+                writer.writerow([row[1]["concept_id"], data[0]])
+
+
+def conceptPrerequisiteGen(dir, concept):
+    with open(dir, 'w', encoding='UTF8', newline='') as a:
+        writer = csv.writer(a)
+        writer.writerow(GRAPH_HEADER)
+        for row in concept.iterrows():
+            for i in range(random.randint(1, 7)):
+                data = dataDictionary.getRandomData(concept)
+                writer.writerow([row[1]["concept_id"], data[0]])
---
+++ main.py
@@ -0,0 +1,223 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+import pandas as pd
+import datagen.dataGen as dataGen
+import argparse
+import sys
+
+SCALE = {"comment": 15347.6069, "concept": 1165.5795, "course": 6.9122, "problem": 4487.0303, "reply": 605.1389,
+         "teacher": 31.1151, "user": 6088.2889, "video": 108.9232}
+resources = ['concept', 'course', 'problem', 'school', 'teacher', 'video', 'course_teacher', 'course_school',
+             'course_field', 'concept_course', 'concept_problem', 'concept_video', 'concept_prerequisites']
+RADIX = 547
+ratio = 0.0025
+RESULT_PATH = "result"
+RELATION_PATH = "/relation"
+DOCUMENT_PATH = "/document"
+KV_PATH = "/kv"
+GRAPH_PATH = "/graph"
+
+COMMENT_ROW_FILENAME = "row_data/comment.csv"
+COMMENT_RESULT_FILENAME = RESULT_PATH + RELATION_PATH + "/comment.csv"
+CONCEPT_ROW_FILENAME = "row_data/concept.csv"
+CONCEPT_RESULT_FILENAME = RESULT_PATH + DOCUMENT_PATH + "/concept.csv"
+COURSE_ROW_FILENAME = "row_data/course.csv"
+COURSE_RESULT_FILENAME = RESULT_PATH + DOCUMENT_PATH + "/course.csv"
+PROBLEM_ROW_FILENAME = "row_data/problem.csv"
+PROBLEM_RESULT_FILENAME = RESULT_PATH + DOCUMENT_PATH + "/problem.csv"
+REPLY_ROW_FILENAME = "row_data/reply.csv"
+REPLY_RESULT_FILENAME = RESULT_PATH + RELATION_PATH + "/reply.csv"
+SCHOOL_ROW_FILENAME = "row_data/school.csv"
+TEACHER_ROW_FILENAME = "row_data/teacher.csv"
+TEACHER_RESULT_FILENAME = RESULT_PATH + RELATION_PATH + "/teacher.csv"
+USER_RESULT_FILENAME = RESULT_PATH + RELATION_PATH + "/user.csv"
+VIDEO_ROW_FILENAME = "row_data/video.csv"
+VIDEO_RESULT_FILENAME = RESULT_PATH + DOCUMENT_PATH + "/video.csv"
+FIELD_ROW_FILENAME = "row_data/field.csv"
+COURSE_TEACHER_RESULT_FILENAME = RESULT_PATH + GRAPH_PATH + "/course_teacher.csv"
+COURSE_FIELD_RESULT_FILENAME = RESULT_PATH + KV_PATH + "/course_field.csv"
+COURSE_COMMENT_RESULT_FILENAME = RESULT_PATH + GRAPH_PATH + "/course_comment.csv"
+USER_COURSE_RESULT_FILENAME = RESULT_PATH + KV_PATH + "/user_course.csv"
+USER_PROBLEM_RESULT_FILENAME = RESULT_PATH + KV_PATH + "/user_problem.csv"
+USER_VIDEO_RESULT_FILENAME = RESULT_PATH + DOCUMENT_PATH + "/user_video.csv"
+USER_COMMENT_RESULT_FILENAME = RESULT_PATH + GRAPH_PATH + "/user_comment.csv"
+USER_REPLY_RESULT_FILENAME = RESULT_PATH + GRAPH_PATH + "/user_reply.csv"
+COMMENT_REPLY_RESULT_FILENAME = RESULT_PATH + GRAPH_PATH + "/comment_reply.csv"
+CONCEPT_COMMENT_RESULT_FILENAME = RESULT_PATH + GRAPH_PATH + "/concept_comment.csv"
+CONCEPT_COURSE_RESULT_FILENAME = RESULT_PATH + GRAPH_PATH + "/concept_course.csv"
+CONCEPT_PROBLEM_RESULT_FILENAME = RESULT_PATH + GRAPH_PATH + "/concept_problem.csv"
+CONCEPT_VIDEO_RESULT_FILENAME = RESULT_PATH + GRAPH_PATH + "/concept_video.csv"
+CONCEPT_PREREQUISITES_RESULT_FILENAME = RESULT_PATH + GRAPH_PATH + "/concept_prerequisites.csv"
+
+if __name__ == "__main__":
+    # 创建命令行参数解析器
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--SF", help="规模因子", type=float)
+    parser.add_argument("--config", help="JSON配置文件")
+    # 解析命令行参数
+    args = parser.parse_args()
+    # 定义数据schema
+    new_ratio = 0
+    if args.SF is None and args.config is None:
+        print("以 SF=0.1 规模生成数据！")
+        new_ratio = ratio
+    elif args.SF is not None and args.config is None:
+        print("以 SF=%f 规模生成数据！" % args.SF)
+        new_ratio = ratio * args.SF / 0.1
+    else:
+        with open(args.config) as f:
+            config_file = json.load(f)
+        if config_file.keys() != SCALE.keys():
+            print("请参照example_config.file填写配置文件")
+            sys.exit(0)
+        for config_key, config_value in config_file.items():
+            if not isinstance(config_value, int):
+                print("请参照example_config.file填写配置文件")
+                sys.exit(0)
+            for everyKey in SCALE:
+                if everyKey == config_key:
+                    SCALE[everyKey] = config_value
+        print("将参照配置文件生成数据！")
+    if new_ratio != 0:
+        for everyKey in SCALE:
+            if everyKey in resources:
+                SCALE[everyKey] = SCALE[everyKey] * RADIX * new_ratio
+            else:
+                SCALE[everyKey] = SCALE[everyKey] / SCALE["user"] * new_ratio * 5000
+    try:
+        if not os.path.exists(RESULT_PATH):
+            os.mkdir(RESULT_PATH)
+        if not os.path.exists(RESULT_PATH + DOCUMENT_PATH):
+            os.mkdir(RESULT_PATH + DOCUMENT_PATH)
+        if not os.path.exists(RESULT_PATH + RELATION_PATH):
+            os.mkdir(RESULT_PATH + RELATION_PATH)
+        if not os.path.exists(RESULT_PATH + GRAPH_PATH):
+            os.mkdir(RESULT_PATH + GRAPH_PATH)
+        if not os.path.exists(RESULT_PATH + KV_PATH):
+            os.mkdir(RESULT_PATH + KV_PATH)
+    except BaseException as e:
+        print(e)
+
+    print("--- start Generating Comment ---")
+    row_data = pd.read_csv(COMMENT_ROW_FILENAME)
+    dataGen.commentGen(COMMENT_RESULT_FILENAME, row_data, SCALE["comment"])
+    print("--- end Generating Comment ---\n\n")
+
+    print("--- start Generating Concept ---")
+    row_data = pd.read_csv(CONCEPT_ROW_FILENAME)
+    dataGen.conceptGen(CONCEPT_RESULT_FILENAME, row_data, SCALE["concept"])
+    print("--- end Generating Concept ---\n\n")
+
+    print("--- start Generating Course ---")
+    row_data = pd.read_csv(COURSE_ROW_FILENAME)
+    dataGen.courseGen(COURSE_RESULT_FILENAME, row_data, SCALE["course"])
+    print("--- end Generating Course ---\n\n")
+
+    print("--- start Generating Problem ---")
+    row_data = pd.read_csv(PROBLEM_ROW_FILENAME)
+    dataGen.problemGen(PROBLEM_RESULT_FILENAME, row_data, SCALE["problem"])
+    print("--- end Generating Problem ---\n\n")
+
+    print("--- start Generating Reply ---")
+    row_data = pd.read_csv(REPLY_ROW_FILENAME)
+    dataGen.replyGen(REPLY_RESULT_FILENAME, row_data, SCALE["reply"])
+    print("--- end Generating Reply ---\n\n")
+
+    print("--- start Generating Teacher ---")
+    row_data = pd.read_csv(TEACHER_ROW_FILENAME)
+    dataGen.teacherGen(TEACHER_RESULT_FILENAME, row_data, SCALE["teacher"])
+    print("--- end Generating Teacher ---\n\n")
+
+    print("--- start Generating User ---")
+    row_data = pd.read_csv(SCHOOL_ROW_FILENAME)
+    dataGen.userGen(USER_RESULT_FILENAME, row_data, SCALE["user"])
+    print("--- end Generating User ---\n\n")
+
+    print("--- start Generating Video ---")
+    row_data = pd.read_csv(VIDEO_ROW_FILENAME)
+    dataGen.videoGen(VIDEO_RESULT_FILENAME, row_data, SCALE["video"])
+    print("--- end Generating Video ---\n\n")
+
+    print("--- start Generating Course_Teacher ---")
+    course = pd.read_csv(COURSE_RESULT_FILENAME)
+    teacher = pd.read_csv(TEACHER_RESULT_FILENAME)
+    dataGen.courseTeacherGen(COURSE_TEACHER_RESULT_FILENAME, course, teacher)
+    print("--- end Generating Course_Teacher ---\n\n")
+
+    print("--- start Generating Course_Field ---")
+    course = pd.read_csv(COURSE_RESULT_FILENAME)
+    field = pd.read_csv(FIELD_ROW_FILENAME)
+    dataGen.courseFiledGen(COURSE_FIELD_RESULT_FILENAME, course, field)
+    print("--- end Generating Course_Field ---\n\n")
+
+    print("--- start Generating Course_Comment ---")
+    course = pd.read_csv(COURSE_RESULT_FILENAME)
+    comment = pd.read_csv(COMMENT_RESULT_FILENAME)
+    dataGen.courseCommentGen(COURSE_COMMENT_RESULT_FILENAME, course, comment)
+    print("--- end Generating Course_Comment ---\n\n")
+
+    print("--- start Generating User_Course ---")
+    user = pd.read_csv(USER_RESULT_FILENAME)
+    course = pd.read_csv(COURSE_RESULT_FILENAME)
+    dataGen.userCourseGen(USER_COURSE_RESULT_FILENAME, user, course)
+    print("--- end Generating User_Course ---\n\n")
+
+    print("--- start Generating User_Problem ---")
+    user = pd.read_csv(USER_RESULT_FILENAME)
+    problem = pd.read_csv(PROBLEM_RESULT_FILENAME)
+    dataGen.userProblemGen(USER_PROBLEM_RESULT_FILENAME, user, problem)
+    print("--- end Generating User_Problem ---\n\n")
+
+    print("--- start Generating User_Video ---")
+    user = pd.read_csv(USER_RESULT_FILENAME)
+    video = pd.read_csv(VIDEO_RESULT_FILENAME)
+    dataGen.userVideoGen(USER_VIDEO_RESULT_FILENAME, user, video)
+    print("--- end Generating User_Video ---\n\n")
+
+    print("--- start Generating User_Comment ---")
+    user = pd.read_csv(USER_RESULT_FILENAME)
+    comment = pd.read_csv(COMMENT_RESULT_FILENAME)
+    dataGen.userCommentGen(USER_COMMENT_RESULT_FILENAME, user, comment)
+    print("--- end Generating User_Comment ---\n\n")
+
+    print("--- start Generating User_Reply ---")
+    user = pd.read_csv(USER_RESULT_FILENAME)
+    reply = pd.read_csv(REPLY_RESULT_FILENAME)
+    dataGen.userReplyGen(USER_REPLY_RESULT_FILENAME, user, reply)
+    print("--- end Generating User_Reply ---\n\n")
+
+    print("--- start Generating Comment_Reply ---")
+    comment = pd.read_csv(COMMENT_RESULT_FILENAME)
+    reply = pd.read_csv(REPLY_RESULT_FILENAME)
+    dataGen.commentReplyGen(COMMENT_REPLY_RESULT_FILENAME, comment, reply)
+    print("--- end Generating Comment_Reply ---\n\n")
+
+    print("--- start Generating Concept_Comment ---")
+    concept = pd.read_csv(CONCEPT_RESULT_FILENAME)
+    comment = pd.read_csv(COMMENT_RESULT_FILENAME)
+    dataGen.conceptCommentGen(CONCEPT_COMMENT_RESULT_FILENAME, concept, comment)
+    print("--- end Generating Concept_Comment ---\n\n")
+
+    print("--- start Generating Concept_Course ---")
+    concept = pd.read_csv(CONCEPT_RESULT_FILENAME)
+    course = pd.read_csv(COURSE_RESULT_FILENAME)
+    dataGen.conceptCourseGen(CONCEPT_COURSE_RESULT_FILENAME, concept, course)
+    print("--- end Generating Concept_Course ---\n\n")
+
+    print("--- start Generating Concept_Problem ---")
+    concept = pd.read_csv(CONCEPT_RESULT_FILENAME)
+    problem = pd.read_csv(PROBLEM_RESULT_FILENAME)
+    dataGen.conceptProblemGen(CONCEPT_PROBLEM_RESULT_FILENAME, concept, problem)
+    print("--- end Generating Concept_Problem ---\n\n")
+
+    print("--- start Generating Concept_Video ---")
+    concept = pd.read_csv(CONCEPT_RESULT_FILENAME)
+    video = pd.read_csv(VIDEO_RESULT_FILENAME)
+    dataGen.conceptVideoGen(CONCEPT_VIDEO_RESULT_FILENAME, concept, video)
+    print("--- end Generating Concept_Video ---\n\n")
+
+    print("--- start Generating Concept_Prerequisite ---")
+    concept = pd.read_csv(CONCEPT_RESULT_FILENAME)
+    dataGen.conceptPrerequisiteGen(CONCEPT_PREREQUISITES_RESULT_FILENAME, concept)
+    print("--- end Generating Concept_Prerequisite ---\n\n")
